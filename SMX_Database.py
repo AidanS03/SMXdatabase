6df6e2cb24f3b0885fa9df560220a9f04accdec4
Astoner <aidanstoner1@gmail.com>
2025-01-12 18:05:55 -0500

    added textbox 4 output

--- SMX_Database.py
+++ SMX_Database.py
@@ -11,9 +11,13 @@ def getRiders():
     # fetch results of query
     myresult = mycursor.fetchall()
 
-    # print query results
+    # Add header for rider info
+    txt.insert(END, "RiderID Number Name Age Class Nationality Points\n")
+
+    # print query results to tkinter window
     for x in myresult:
-        print(x)
+        txt.insert(END, x)
+        txt.insert(END, "\n")
 
 # connect to a database
 mydb = mysql.connector.connect(
@@ -28,10 +32,15 @@ mycursor = mydb.cursor()
 # set up tkinter window
 root = Tk()
 root.geometry("700x350")
+root.title("2025 SMX Database")
+
 frame = ttk.Frame(root, padding=10)
 frame.grid()
 ttk.Label(frame, text="Hello World").grid(column=0, row=0)
 ttk.Button(frame, text="Quit", command=root.destroy).grid(column=2, row=0)
 ttk.Button(frame, text="Get Riders", command=getRiders).grid(column=1, row=0)
+
+txt = Text(root, height=10, width=60)
+txt.grid(column=2, row=1)
 root.mainloop()
 
